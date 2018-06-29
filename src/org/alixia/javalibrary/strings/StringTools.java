716277fb301ef96ef716763feae8899b6938f902
Zeale <zeale@dusttoash.org>
2018-06-29 15:10:51 -0500

    Fixed some containsIgnoreCase crap
    So since possible matches were taken out of the "standbies" pool when a potential match was found, if another potential match was there while the previous one was still being checked, the new potential match would be ignored (since there was nothing matching it in the standbies pool). The old potential match may have been found to not be a match, thus, the actual match would be ignored.
    
    Also there was a character out of bounds error thing. yay.

--- src/org/alixia/javalibrary/strings/StringTools.java
+++ src/org/alixia/javalibrary/strings/StringTools.java
@@ -2,6 +2,8 @@ package org.alixia.javalibrary.strings;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -9,35 +11,56 @@ import java.util.Map.Entry;
 import org.alixia.chatroom.api.QuickList;
 
 public final class StringTools {
+	/**
+	 * Returns true if any of <code>possiblePieces</code> are contained inside the
+	 * given String, <code>string</code>.
+	 * 
+	 * @param string         The string that may contain something in
+	 *                       <code>possiblePieces</code>.
+	 * @param possiblePieces Any number of Strings. If <code>string</code> contains
+	 *                       one of these, this method will return true.
+	 * @return <code>true</code> if one of the <code>possiblePieces</code> are found
+	 *         inside <code>string</code>. False otherwise.
+	 */
 	public static boolean containsIgnoreCase(String string, String... possiblePieces) {
 
 		Map<String, Integer> matchingPieces = new HashMap<>(0);
-		List<String> standbies = new QuickList<>(possiblePieces), mute = new ArrayList<>();
+		List<String> standbies = new LinkedList<>(), mute = new ArrayList<>();
+		for (String s : possiblePieces)
+			if (s == null || s.isEmpty())
+				continue;
+			else
+				standbies.add(s.toLowerCase());
 
 		for (char c : string.toCharArray()) {
-			for (Entry<String, Integer> e : matchingPieces.entrySet()) {
+			for (Iterator<Entry<String, Integer>> iterator = matchingPieces.entrySet().iterator(); iterator
+					.hasNext();) {
+				Entry<String, Integer> e = iterator.next();
 				char nextChar = e.getKey().charAt(e.getValue());
-				if (nextChar == Character.toLowerCase(c) || nextChar == Character.toUpperCase(c)) {
+				if (nextChar == c) {
 					e.setValue(e.getValue() + 1);
 					if (e.getValue() == e.getKey().length())
 						return true;
-				} else {
-					mute.add(e.getKey());
-				}
+				} else
+					iterator.remove();
 			}
 			for (String s : standbies) {
 				char firstChar = s.charAt(0);
-				if (firstChar == Character.toLowerCase(c) || firstChar == Character.toUpperCase(c))
-					matchingPieces.put(s, 1);
+				if (firstChar == c)
+					if (s.length() == 1)
+						return true;
+					else
+						matchingPieces.put(s, 1);
 			}
-			for (String s : mute) {
-				standbies.add(s);
-				matchingPieces.remove(s);
-			}
-			mute.clear();
 		}
 
 		return false;
+	}
+
+	public static void main(String[] args) {
+		// TODO We had an issue...
+		System.out.println(containsIgnoreCase("ststrong", "strong"));// false
+		// I fixed it doe. :)
 	}
 
 	public static boolean equalsAny(final String arg, final String... possibleMatches) {
