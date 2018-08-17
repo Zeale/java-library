2da42cacf41a81075929616bc2b07c67184b37eb
Zeale <Zeale@dusttoash.org>
2018-08-17 02:30:25 -0500

    Updated StringTools

--- src/org/alixia/javalibrary/strings/StringTools.java
+++ src/org/alixia/javalibrary/strings/StringTools.java
@@ -6,13 +6,29 @@ import java.util.List;
 
 public final class StringTools {
 	/**
+	 * <p>
 	 * Returns true if any of <code>possiblePieces</code> are contained inside the
 	 * given String, <code>string</code>.
+	 * </p>
+	 * <p>
+	 * Note: This method tends to be faster than comparing possible matches and the
+	 * base string with {@link String#contains(CharSequence)}, after calling
+	 * {@link String#toLowerCase()} upon all of them, if the base string contains
+	 * fragments of the possible pieces, (where the fragments start with the same
+	 * characters as their respective pieces), or if any of the possible pieces are
+	 * near a long list of possible pieces, in comparison to a for loop. The latter
+	 * is due to the fact that this method checks against all the pieces
+	 * simultaneously, so if a match is near the end of <code>possiblePieces</code>,
+	 * it doesn't have to wait for all the other possible pieces to be checked
+	 * before it can be checked.
+	 * </p>
 	 * 
-	 * @param string         The string that may contain something in
-	 *                       <code>possiblePieces</code>.
-	 * @param possiblePieces Any number of Strings. If <code>string</code> contains
-	 *                       one of these, this method will return true.
+	 * @param string
+	 *            The string that may contain something in
+	 *            <code>possiblePieces</code>.
+	 * @param possiblePieces
+	 *            Any number of Strings. If <code>string</code> contains one of
+	 *            these, this method will return true.
 	 * @return <code>true</code> if one of the <code>possiblePieces</code> are found
 	 *         inside <code>string</code>. False otherwise.
 	 */
@@ -60,6 +76,8 @@ public final class StringTools {
 	}
 
 	public static boolean equalsAny(final String arg, final String... possibleMatches) {
+		if (arg == null)
+			return false;
 		for (final String s : possibleMatches)
 			if (arg.equals(s))
 				return true;
@@ -67,6 +85,8 @@ public final class StringTools {
 	}
 
 	public static boolean equalsAnyIgnoreCase(final String arg, final String... possibleMatches) {
+		if (arg == null)
+			return false;
 		for (final String s : possibleMatches)
 			if (arg.equalsIgnoreCase(s))
 				return true;
