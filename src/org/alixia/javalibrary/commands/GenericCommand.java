0efb0934598e2db8e921f4d2c458cbdf47282812
Zeale <zeale@dusttoash.org>
2019-01-05 23:08:56 -0600

    Updated generic command manager api

--- src/org/alixia/javalibrary/commands/GenericCommand.java
+++ src/org/alixia/javalibrary/commands/GenericCommand.java
@@ -2,6 +2,4 @@ package org.alixia.javalibrary.commands;
 
 public interface GenericCommand<D> extends GenericCommandConsumer<D> {
 	boolean match(D data);
-
-	void act(D data);
 }
--- src/org/alixia/javalibrary/commands/GenericCommandManager.java
+++ src/org/alixia/javalibrary/commands/GenericCommandManager.java
@@ -1,14 +1,71 @@
 package org.alixia.javalibrary.commands;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class GenericCommandManager<D> {
 	private final List<GenericCommand<? super D>> commands = new LinkedList<>();
+	private final Stack<GenericCommandConsumer<? super D>> consumers = new Stack<>();
+	private final Stack<OptionalGenericCommandConsumer<? super D>> optionalConsumers = new Stack<>();
 
-	public void run(D data) {
+	public void addCommand(GenericCommand<? super D> command) {
+		if (!commands.contains(command))
+			commands.add(command);
+	}
+
+	public void removeCommand(GenericCommand<? super D> command) {
+		commands.remove(command);
+	}
+
+	public void addConsumer(GenericCommandConsumer<? super D> consumer) {
+		consumers.push(consumer);
+	}
+
+	public void removeConsumer(GenericCommandConsumer<? super D> consumer) {
+		consumers.remove(consumer);
+	}
+
+	public void addConsumer(OptionalGenericCommandConsumer<? super D> consumer) {
+		addOptionalConsumer(consumer);
+	}
+
+	public void removeConsumer(OptionalGenericCommandConsumer<? super D> consumer) {
+		removeOptionalConsumer(consumer);
+	}
+
+	public void addOptionalConsumer(OptionalGenericCommandConsumer<? super D> consumer) {
+		optionalConsumers.push(consumer);
+	}
+
+	public void removeOptionalConsumer(OptionalGenericCommandConsumer<? super D> consumer) {
+		optionalConsumers.remove(consumer);
+	}
+
+	public boolean run(D data) {
+
+		// Optional consumers are checked,
+		if (!optionalConsumers.isEmpty())
+			for (Iterator<OptionalGenericCommandConsumer<? super D>> iterator = optionalConsumers.iterator(); iterator
+					.hasNext();)
+				if (iterator.next().act(data)) {
+					iterator.remove();
+					return true;
+				}
+
+		// then regular consumers are checked,
+		if (!consumers.isEmpty()) {
+			consumers.pop().act(data);
+			return true;
+		}
+
+		// then commands are checked.
 		for (GenericCommand<? super D> gc : commands)
-			if (gc.match(data))
+			if (gc.match(data)) {
 				gc.act(data);
+				return true;
+			}
+		return false;
 	}
 }
