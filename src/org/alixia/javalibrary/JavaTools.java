a547b11bdfea3eba7da71cc41437cb10da6f8887
Zeale <zeale@dusttoash.org>
2019-01-07 00:24:55 -0600

    Added unmodifiable stack to JavaTool

--- src/org/alixia/javalibrary/JavaTools.java
+++ src/org/alixia/javalibrary/JavaTools.java
@@ -1,5 +1,16 @@
 package org.alixia.javalibrary;
 
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.RandomAccess;
+import java.util.Spliterator;
+import java.util.Stack;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+
 public final class JavaTools {
 	private JavaTools() {
 	}
@@ -7,6 +18,256 @@ public final class JavaTools {
 	@SafeVarargs
 	public static final <T> T pickRandomElement(T... ts) {
 		return ts[(int) (Math.random() * ts.length)];
+	}
+
+	public static <E> ListIterator<E> unmodifyingListIterator(ListIterator<E> base) {
+		return new ListIterator<E>() {
+
+			@Override
+			public boolean hasNext() {
+				return base.hasNext();
+			}
+
+			@Override
+			public E next() {
+				return base.next();
+			}
+
+			@Override
+			public boolean hasPrevious() {
+				return base.hasNext();
+			}
+
+			@Override
+			public E previous() {
+				return base.previous();
+			}
+
+			@Override
+			public int nextIndex() {
+				return base.nextIndex();
+			}
+
+			@Override
+			public int previousIndex() {
+				return base.previousIndex();
+			}
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException(
+						"This ListIterator can't modify its underlying Collection; that operation is invalid.");
+			}
+
+			@Override
+			public void set(E e) {
+				throw new UnsupportedOperationException(
+						"This ListIterator can't modify its underlying Collection; that operation is invalid.");
+			}
+
+			@Override
+			public void add(E e) {
+				throw new UnsupportedOperationException(
+						"This ListIterator can't modify its underlying Collection; that operation is invalid.");
+			}
+		};
+	}
+
+	public static <E> Iterator<E> unmodifyingIterator(Iterator<E> itr) {
+		return new Iterator<E>() {
+
+			@Override
+			public boolean hasNext() {
+				return itr.hasNext();
+			}
+
+			@Override
+			public E next() {
+				return itr.next();
+			}
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException(
+						"This Iterator can't modify its underlying Collection; that operation is invalid.");
+			}
+		};
+	}
+
+	public static <E> Stack<E> unmodifiableStack(Stack<E> stack) {
+
+		return new Stack<E>() {
+
+			/**
+			 * SUID
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public synchronized void trimToSize() {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized void ensureCapacity(int minCapacity) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized void setSize(int newSize) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized void setElementAt(E obj, int index) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized void removeElementAt(int index) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized void insertElementAt(E obj, int index) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized void addElement(E obj) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized boolean removeElement(Object obj) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized void removeAllElements() {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized E set(int index, E element) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized boolean add(E e) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public boolean remove(Object o) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public void add(int index, E element) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized E remove(int index) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public void clear() {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized boolean addAll(Collection<? extends E> c) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized boolean removeAll(Collection<?> c) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized boolean retainAll(Collection<?> c) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized boolean addAll(int index, Collection<? extends E> c) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized List<E> subList(int fromIndex, int toIndex) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			protected synchronized void removeRange(int fromIndex, int toIndex) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized ListIterator<E> listIterator(int index) {
+				return unmodifyingListIterator(super.listIterator(index));
+			}
+
+			@Override
+			public synchronized ListIterator<E> listIterator() {
+				return unmodifyingListIterator(super.listIterator());
+			}
+
+			@Override
+			public synchronized Iterator<E> iterator() {
+				return unmodifyingIterator(super.iterator());
+			}
+
+			@Override
+			public synchronized boolean removeIf(Predicate<? super E> filter) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized void replaceAll(UnaryOperator<E> operator) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public synchronized void sort(Comparator<? super E> c) {
+				throw new UnsupportedOperationException(
+						"This Stack is unmodifiable; the requested operation is invalid.");
+			}
+
+			@Override
+			public Spliterator<E> spliterator() {
+				return super.spliterator();
+			}
+
+		};
 	}
 
 }
