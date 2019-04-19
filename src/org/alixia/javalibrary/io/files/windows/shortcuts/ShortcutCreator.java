8da838be8f688ed8ab182691545c9495debc5a34
Zeale <zeale@dusttoash.org>
2019-04-18 19:31:19 -0500

    Updated ShortcutCreator

--- src/org/alixia/javalibrary/io/files/windows/shortcuts/ShortcutCreator.java
+++ src/org/alixia/javalibrary/io/files/windows/shortcuts/ShortcutCreator.java
@@ -23,11 +23,12 @@ public class ShortcutCreator {
 			throws FileNotFoundException, IOException {
 
 		try (DataOutputStream o = new DataOutputStream(out)) {
-			o.write(new byte[] { 0, 0, 0, 0x4C });// Header Size
+			o.writeInt(0x4C);// Header Size
 			o.write(new byte[] { 0, 2, 0x14, 1, 0, 0, 0, 0, (byte) 0xC0, 0, 0, 0, 0, 0, 0, 0x46 });// LinkCLSID
 
 			// Link Flags
-			byte flg1 = 1;// Shell Link contains Unicode encoded strings. (IsUnicode)
+			byte flg1 = (byte) 0b10000001;// HasLinkTargetIDList and Shell Link contains Unicode encoded strings.
+											// (IsUnicode)
 			if (description != null)
 				flg1 |= 0b00100000;// Add a shortcut description. (HasName)
 			if (icon != null)
@@ -36,8 +37,28 @@ public class ShortcutCreator {
 
 			// File Attributes
 			o.write(new byte[] { 0b00000010, 0, 0, 0 });// FILE_ATTRIBUTE_NORMAL
-			
-			
+
+			o.writeLong(0);// CreationTime
+			o.writeLong(0);// AccessTime
+			o.writeLong(0);// WriteTime
+			o.writeInt(0);// FileSize
+
+			o.writeInt(0);// Icon Index
+			o.write(1);// Show Command
+
+			// HotKeyFlags
+			o.writeByte(0x53);// LowByte
+			o.writeByte(0b00000111);// HighByte
+
+			// The above should result in Ctrl + Shift + Alt being the hotkey used to open
+			// the application.
+
+			// Now for reserved blocks.
+			o.writeShort(0);
+			o.writeLong(0);
+
+			// This completes the SHELL_LINK_HEADER. Now we need to fill out the
+			// LINKTARGET_IDLIST.
 
 		}
 
