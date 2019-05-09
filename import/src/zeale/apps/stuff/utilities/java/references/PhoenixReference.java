cdf6d330682d275bd7c59c99dd10f84979cbc4c7
Zeale <zeale@dusttoash.org>
2019-05-08 23:53:20 -0500

    Added PhoenixReference

---
+++ import/src/zeale/apps/stuff/utilities/java/references/PhoenixReference.java
@@ -0,0 +1,37 @@
+package zeale.apps.stuff.utilities.java.references;
+
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
+import java.util.concurrent.Callable;
+
+public abstract class PhoenixReference<T> {
+
+	private WeakReference<T> reference;
+
+	protected abstract T generate();
+
+	{
+		reference = new WeakReference<>(generate());
+	}
+
+	public static <T> PhoenixReference<? super T> create(java.util.function.Supplier<? extends T> generator) {
+		return new PhoenixReference<T>() {
+			@Override
+			protected T generate() {
+				return generator.get();
+			}
+		};
+	}
+
+	public final T get() {
+		// #reference should always have a value.
+		T value = reference.get();
+		if (value == null) {
+			T newValue = generate();
+			reference = new WeakReference<>(newValue);
+			return newValue;
+		} else
+			return value;
+	}
+
+}
