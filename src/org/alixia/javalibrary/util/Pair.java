3043e52dc6a15b629a2559059fb3211f2a2a4fc9
Zeale <zeale@dusttoash.org>
2019-06-27 17:49:18 -0500

    Added the Pair util class

---
+++ src/org/alixia/javalibrary/util/Pair.java
@@ -0,0 +1,14 @@
+package org.alixia.javalibrary.util;
+
+public class Pair<F, S> {
+	public F first;
+	public S second;
+
+	public Pair() {
+	}
+
+	public Pair(F first, S second) {
+		this.first = first;
+		this.second = second;
+	}
+}
