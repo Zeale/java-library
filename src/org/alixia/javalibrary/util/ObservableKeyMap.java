800d1bdfcae9da9c1713bef032adba15a136e194
Zeale <zeale@dusttoash.org>
2019-07-09 00:00:57 -0500

    Added ObservableKeyMap class (WIP)

---
+++ src/org/alixia/javalibrary/util/ObservableKeyMap.java
@@ -0,0 +1,36 @@
+package org.alixia.javalibrary.util;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+import org.alixia.javalibrary.util.KeyMap.Key;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableMap;
+
+public class ObservableKeyMap<V, M extends ObservableMap<Key<?>, V>> extends KeyMap<V, M> {
+
+	public ObservableKeyMap(M map) {
+		super(map);
+	}
+
+	public static <V, M extends ObservableMap<Key<?>, V>> ObservableKeyMap<V, M> observableKeyMap(M map) {
+		return new ObservableKeyMap<>(map);
+	}
+
+	public static <V> ObservableKeyMap<V, ObservableMap<KeyMap.Key<?>, V>> observableKeyMap() {
+		return observableKeyMap(new WeakHashMap<>());
+	}
+
+	public static <V> ObservableKeyMap<V, ObservableMap<KeyMap.Key<?>, V>> observableKeyMap(Map<Key<?>, V> map) {
+		return observableKeyMap(FXCollections.observableMap(map));
+	}
+
+	// TODO
+
+	/**
+	 * SUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
