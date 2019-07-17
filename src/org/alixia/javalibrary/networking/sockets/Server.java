a43bacf01353a018ce36fb33ad076834f4a98836
Zeale <zeale@dusttoash.org>
2019-07-17 17:46:38 -0500

    Added a Server class
    This is a wip

---
+++ src/org/alixia/javalibrary/networking/sockets/Server.java
@@ -0,0 +1,11 @@
+package org.alixia.javalibrary.networking.sockets;
+
+import java.net.ServerSocket;
+
+public class Server {
+	private final ServerSocket server;
+
+	public Server(ServerSocket server) {
+		this.server = server;
+	}
+}
