ff11bb8142e855baf4e9afe1d548f63108d0ab95
Zeale <zeale@dusttoash.org>
2019-10-10 15:32:56 -0500

    Add CharacterParser type

---
+++ src/org/alixia/javalibrary/streams/CharacterParser.java
@@ -0,0 +1,34 @@
+package org.alixia.javalibrary.streams;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public interface CharacterParser extends CharacterStream {
+	/**
+	 * Returns the last value returned by {@link #next()}, or throws an exception if
+	 * none exists.
+	 * 
+	 * @return The current character that this parser is on.
+	 */
+	int curr() throws RuntimeException;
+
+	static CharacterParser from(Reader reader) {
+		return new CharacterParser() {
+			int curr;
+
+			@Override
+			public int next() {
+				try {
+					return curr = reader.read();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+
+			@Override
+			public int curr() throws RuntimeException {
+				return curr;
+			}
+		};
+	}
+}
--- src/org/alixia/javalibrary/streams/CharacterStream.java
+++ src/org/alixia/javalibrary/streams/CharacterStream.java
@@ -1,5 +1,8 @@
 package org.alixia.javalibrary.streams;
 
+import java.io.IOException;
+import java.io.Reader;
+
 public interface CharacterStream {
 	default Character nextChar() {
 		int nextch = next();
@@ -15,8 +18,8 @@ public interface CharacterStream {
 	 */
 	int next();
 
-	static CharacterStream from(String string) {
-		return new CharacterStream() {
+	static CharacterParser from(String string) {
+		return new CharacterParser() {
 
 			private int pos = 0;
 
@@ -24,6 +27,31 @@ public interface CharacterStream {
 			public int next() {
 				return pos >= string.length() ? -1 : string.codePointAt(pos++);
 			}
+
+			@Override
+			public int curr() throws RuntimeException {
+				return string.codePointAt(pos);
+			}
 		};
 	}
+
+	/**
+	 * Returns a {@link CharacterStream} that wraps a {@link Reader}. The stream
+	 * will delegate its methods to the reader's {@link Reader#read()} method,
+	 * throwing a {@link RuntimeException} that wraps any checked exception thrown
+	 * by the call to {@link Reader#read()}.
+	 * 
+	 * @param reader The {@link Reader} to wrap.
+	 * @return A new {@link CharacterStream}.
+	 */
+	static CharacterStream from(Reader reader) {
+		return () -> {
+			try {
+				return reader.read();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		};
+	}
+
 }
