bcfe9e34091385cd02f4e2029b99855e5dbce619
Gartham <gartham@dusttoash.org>
2020-07-09 05:17:36 -0500

    Add short toString function for JSON

--- src/org/alixia/javalibrary/json/JSONValue.java
+++ src/org/alixia/javalibrary/json/JSONValue.java
@@ -2,6 +2,8 @@ package org.alixia.javalibrary.json;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
 public interface JSONValue extends Serializable {
 	String toString(String indentation);
@@ -17,6 +19,33 @@ public interface JSONValue extends Serializable {
 	static <T extends Collection<? super String>> void addAll(Collection<? extends JSONValue> c, T t) {
 		for (JSONValue j : c)
 			t.add(j.toString());
+	}
+
+	public static String toStringShort(JSONValue v) {
+		if (v instanceof JSONArray) {
+			StringBuilder sb = new StringBuilder("[");
+			Iterator<JSONValue> iterator = ((JSONArray) v).iterator();
+			if (iterator.hasNext()) {
+				sb.append(toStringShort(iterator.next()));
+				for (; iterator.hasNext();)
+					sb.append(',').append(toStringShort(iterator.next()));
+			}
+			sb.append(']');
+			return sb.toString();
+		} else if (v instanceof JSONObject) {
+			StringBuilder sb = new StringBuilder("{");
+			Iterator<Entry<String, JSONValue>> iterator = ((JSONObject) v).entrySet().iterator();
+			if (iterator.hasNext()) {
+				Entry<String, JSONValue> item = iterator.next();
+				sb.append('"').append(escape(item.getKey())).append("\":").append(toStringShort(item.getValue()));
+				for (; iterator.hasNext();)
+					sb.append(',').append('"').append(escape((item = iterator.next()).getKey())).append("\":")
+							.append(toStringShort(item.getValue()));
+			}
+			sb.append('}');
+			return sb.toString();
+		} else
+			return v.toString();
 	}
 
 	static String escape(String input) {
