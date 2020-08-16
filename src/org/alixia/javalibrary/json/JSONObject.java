43dc82e75c81a8c6eb3db505228ec6f42a16aba8
Gartham <gartham@dusttoash.org>
2020-08-16 01:41:54 -0500

    Add convenience methods to JSONObject

--- src/org/alixia/javalibrary/json/JSONObject.java
+++ src/org/alixia/javalibrary/json/JSONObject.java
@@ -24,8 +24,8 @@ public class JSONObject extends HashMap<String, JSONValue> implements JSONValue 
 		else {
 			Iterator<Entry<String, JSONValue>> iterator = entrySet().iterator();
 			Entry<String, JSONValue> e = iterator.next();
-			builder.append('\n').append(indentation).append('\t').append('"').append(JSONValue.escape(e.getKey())).append('"').append(':')
-					.append(e.getValue().toString(indentation + '\t'));
+			builder.append('\n').append(indentation).append('\t').append('"').append(JSONValue.escape(e.getKey()))
+					.append('"').append(':').append(e.getValue().toString(indentation + '\t'));
 			for (; iterator.hasNext();) {
 				e = iterator.next();
 				builder.append(",\n").append(indentation).append('\t').append('"').append(e.getKey()).append('"')
@@ -36,6 +36,41 @@ public class JSONObject extends HashMap<String, JSONValue> implements JSONValue 
 		builder.append(indentation).append('}');
 
 		return builder.toString();
+	}
+
+	public JSONString getJString(String key) {
+		return (JSONString) get(key);
+	}
+
+	public String getString(String key) {
+		return getJString(key).getValue();
+	}
+
+	public JSONNumber getJNumber(String key) {
+		return (JSONNumber) get(key);
+	}
+
+	public int getInt(String key) {
+		return getJNumber(key).intValue();
+	}
+
+	public long getLong(String key) {
+		return getJNumber(key).longValue();
+	}
+
+	public JSONObject put(String key, String value) {
+		put(key, new JSONString(value));
+		return this;
+	}
+
+	public JSONObject put(String key, int value) {
+		put(key, new JSONNumber(value));
+		return this;
+	}
+
+	public JSONObject put(String key, long value) {
+		put(key, new JSONNumber(value));
+		return this;
 	}
 
 }
