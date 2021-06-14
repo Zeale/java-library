94c59bb1f0a7ae8544fc2591e67952c74055ae60
Gartham <Gartham@dusttoash.org>
2021-06-14 03:22:05 -0500

    Update Parser.java

--- import/src/zeale/mouse/utils/Parser.java
+++ import/src/zeale/mouse/utils/Parser.java
@@ -1,7 +1,17 @@
 package zeale.mouse.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface Parser<T> {
 	T peek();
 
 	T next();
+
+	default List<T> parseAll() {
+		List<T> el = new ArrayList<>();
+		while (peek() != null)
+			el.add(next());
+		return el;
+	}
 }
